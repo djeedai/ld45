1a46ffc52066ec4458047d4900c6bf803c61986e
Jerome Humbert <jerome.humbert@gmail.com>
2019-10-06 22:15:06 +0100

    Level 7 flavor text

---
+++ Assets/Scripts/Level7.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Level7 : Level
+{
+    public override IEnumerator StartLevel()
+    {
+        {
+            var dialog = new TextDialog();
+            dialog.Snippets.Add(new TextSnippet { text = "Finally some color appeared." });
+            yield return Director.Instance.WaitForDialog(dialog, block: false);
+        }
+
+        yield return new WaitForSeconds(1.0f);
+    }
+}
